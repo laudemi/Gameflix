45356f2e94b64d478935ab5ece8a767c4b963ad3
Laudemi Oliveira <39411334+laudemi@users.noreply.github.com>
2021-11-27 09:05:50 -0300

    Add files via upload

---
+++ js/games.js
@@ -0,0 +1,6 @@
+
+var info = document.getElementsByClassName('titulo');
+var info = document.querySelector('.titulo');
+info.addEventListener('click',function(){
+    info.innerHTML = "Call of Duty";
+});
